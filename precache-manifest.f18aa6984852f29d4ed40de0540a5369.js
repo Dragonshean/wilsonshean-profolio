686e3d889dd1982fa6d00e8d221d3c39955b63d1
dargonshean <dragonshean@gmail.com>
2022-12-13 15:28:09 +0800

    Updates

---
+++ precache-manifest.f18aa6984852f29d4ed40de0540a5369.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a2ccd332f2f2a9f21f",
+    "url": "/wilsonshean-profolio/static/css/main.0efc1abc.chunk.css"
+  },
+  {
+    "revision": "02a2ccd332f2f2a9f21f",
+    "url": "/wilsonshean-profolio/static/js/main.02a2ccd3.chunk.js"
+  },
+  {
+    "revision": "47feb2933457be6d9b87",
+    "url": "/wilsonshean-profolio/static/js/1.47feb293.chunk.js"
+  },
+  {
+    "revision": "deb436ba5fc60581c7b0",
+    "url": "/wilsonshean-profolio/static/css/2.dca6a5c5.chunk.css"
+  },
+  {
+    "revision": "deb436ba5fc60581c7b0",
+    "url": "/wilsonshean-profolio/static/js/2.deb436ba.chunk.js"
+  },
+  {
+    "revision": "ab205cc6bef62ed04fae",
+    "url": "/wilsonshean-profolio/static/js/runtime~main.ab205cc6.js"
+  },
+  {
+    "revision": "9a085815101433658481d6faa0973048",
+    "url": "/wilsonshean-profolio/static/media/Drashean_logo.9a085815.png"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/wilsonshean-profolio/static/media/github-1.1bb02710.svg"
+  },
+  {
+    "revision": "51f42ec568eb2bdbc734ad787f2ae037",
+    "url": "/wilsonshean-profolio/static/media/facebook-4.51f42ec5.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/wilsonshean-profolio/static/media/linkedin-3.5b238434.svg"
+  },
+  {
+    "revision": "6961f8a90f485638d761aba5d5941c7c",
+    "url": "/wilsonshean-profolio/static/media/coding.6961f8a9.jpeg"
+  },
+  {
+    "revision": "425c8ac6e44355e2537008b6fce21c5e",
+    "url": "/wilsonshean-profolio/static/media/Language.425c8ac6.jpg"
+  },
+  {
+    "revision": "019e9da22e21e2c6ec7d4c24f1c3a917",
+    "url": "/wilsonshean-profolio/static/media/graphic-design-tools.019e9da2.jpeg"
+  },
+  {
+    "revision": "4f075bf80508388295a5523673121b1a",
+    "url": "/wilsonshean-profolio/static/media/About me.4f075bf8.jpg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/wilsonshean-profolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/wilsonshean-profolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "8a802cc49c53bfaa9136e4989073228a",
+    "url": "/wilsonshean-profolio/static/media/project1.8a802cc4.jpg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/wilsonshean-profolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "c99cae385c76148238608d037cc53d83",
+    "url": "/wilsonshean-profolio/static/media/DynaPuff-Bold.c99cae38.ttf"
+  },
+  {
+    "revision": "f7138cf00b111282887296a97c3e0162",
+    "url": "/wilsonshean-profolio/static/media/DynaPuff-Medium.f7138cf0.ttf"
+  },
+  {
+    "revision": "4af48454b51ec0e98cf0c4970c981afe",
+    "url": "/wilsonshean-profolio/static/media/DynaPuff-Regular.4af48454.ttf"
+  },
+  {
+    "revision": "b4fe0adf8e016474d1572569cdc913eb",
+    "url": "/wilsonshean-profolio/static/media/colorful-abstract-nebula-space-background.b4fe0adf.jpg"
+  },
+  {
+    "revision": "f9714699ef669227abcaa36e0955e77a",
+    "url": "/wilsonshean-profolio/static/media/project2.f9714699.JPG"
+  },
+  {
+    "revision": "031c54a53ecc0b31b91731db072064ef",
+    "url": "/wilsonshean-profolio/static/media/project3.031c54a5.jpg"
+  },
+  {
+    "revision": "cab9a8ff0adead7aa0278b12e76c16e3",
+    "url": "/wilsonshean-profolio/static/media/project4.cab9a8ff.JPG"
+  },
+  {
+    "revision": "332ed2bfd0ffd170088fae94838345bc",
+    "url": "/wilsonshean-profolio/static/media/project5.332ed2bf.JPG"
+  },
+  {
+    "revision": "e36ab3f45f6df0da1e9468e1c64db2bd",
+    "url": "/wilsonshean-profolio/static/media/project6.e36ab3f4.JPG"
+  },
+  {
+    "revision": "f34e503dd27f4322865c1ca8b20a9343",
+    "url": "/wilsonshean-profolio/static/media/contact.f34e503d.jpg"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/wilsonshean-profolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/wilsonshean-profolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/wilsonshean-profolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "d6b5e675313259b529ce1946e0048194",
+    "url": "/wilsonshean-profolio/index.html"
+  }
+];
