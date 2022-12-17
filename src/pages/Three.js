fc4edaa4db27a0be7dc4c82345be239cc1f1aab8
dargonshean <dragonshean@gmail.com>
2022-12-18 01:05:13 +0800

    fix again

--- src/pages/Three.js
+++ src/pages/Three.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react' //引入一個 Suspense 給3D 因為有用useLoader 要用這個去fetch
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei' //加入可旋轉3D的function
 import { Model } from '../components/Kratoshouse'
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
