88d258446f7af985cab615c2a5ab3467652b6734
dargonshean <dragonshean@gmail.com>
2022-12-18 00:12:54 +0800

    add blender 3D module

--- src/pages/Three.js
+++ src/pages/Three.js
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react' //引入一個 Suspense 給3D 因為有用useLoader 要用這個去fetch
 import { Canvas } from '@react-three/fiber'
-import { Box3D } from '../components/Box3D'
 import { OrbitControls } from '@react-three/drei' //加入可旋轉3D的function
+import { Model } from '../components/Kratoshouse'
+
 
 
 
@@ -11,14 +12,18 @@ function Three () {
 
 
 
+
         
     return (
-        <Canvas className='Box3D-container'>
-            <OrbitControls enableZoom={false} />
+        <Canvas 
+            className='Box3D-container' 
+            camera={{position: [-50, 30, 10], fov: 30}}        
+        >
+            <OrbitControls enableZoom={true} />
             <ambientLight intensity={0.5} />
             <directionalLight position={[-2, 5, 2]} intensity={1} />
             <Suspense fallback={null}>
-                <Box3D/>
+                <Model />
             </Suspense>
         </Canvas>
     )
